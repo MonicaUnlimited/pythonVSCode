b83a543afd4cbca59e5efacc7493225d6e76b100
Don Jayamanne <don.jayamanne@yahoo.com>
2018-05-02 09:15:25 -0700

    :bug: fix tests

--- src/test/interpreters/virtualEnvManager.test.ts
+++ src/test/interpreters/virtualEnvManager.test.ts
@@ -4,12 +4,14 @@
 import { expect } from 'chai';
 import { Container } from 'inversify';
 import * as TypeMoq from 'typemoq';
+import { IWorkspaceService } from '../../client/common/application/types';
 import { FileSystem } from '../../client/common/platform/fileSystem';
 import { PlatformService } from '../../client/common/platform/platformService';
 import { IFileSystem, IPlatformService } from '../../client/common/platform/types';
 import { BufferDecoder } from '../../client/common/process/decoder';
 import { ProcessService } from '../../client/common/process/proc';
 import { IBufferDecoder, IProcessService } from '../../client/common/process/types';
+import { IPipEnvService } from '../../client/interpreter/contracts';
 import { VirtualEnvironmentManager } from '../../client/interpreter/virtualEnvs';
 import { ServiceContainer } from '../../client/ioc/container';
 import { ServiceManager } from '../../client/ioc/serviceManager';
@@ -34,7 +36,9 @@ suite('Virtual environment manager', () => {
     serviceManager.addSingleton<IProcessService>(IProcessService, ProcessService);
     serviceManager.addSingleton<IBufferDecoder>(IBufferDecoder, BufferDecoder);
     serviceManager.addSingleton<IFileSystem>(IFileSystem, FileSystem);
-    serviceManager.addSingleton<IPlatformService>(IFileSystem, PlatformService);
+    serviceManager.addSingleton<IPlatformService>(IPlatformService, PlatformService);
+    serviceManager.addSingletonInstance<IPipEnvService>(IPipEnvService, TypeMoq.Mock.ofType<IPipEnvService>().object);
+    serviceManager.addSingletonInstance<IWorkspaceService>(IWorkspaceService, TypeMoq.Mock.ofType<IWorkspaceService>().object);
     const venvManager = new VirtualEnvironmentManager(serviceContainer);
     const name = await venvManager.getEnvironmentName(PYTHON_PATH);
     const result = name === '' || name === 'venv' || name === 'virtualenv';
@@ -43,6 +47,9 @@ suite('Virtual environment manager', () => {
 
   async function testSuffix(expectedName: string) {
     process = TypeMoq.Mock.ofType<IProcessService>();
+    serviceManager.addSingletonInstance<IFileSystem>(IFileSystem, TypeMoq.Mock.ofType<IFileSystem>().object);
+    serviceManager.addSingletonInstance<IPipEnvService>(IPipEnvService, TypeMoq.Mock.ofType<IPipEnvService>().object);
+    serviceManager.addSingletonInstance<IWorkspaceService>(IWorkspaceService, TypeMoq.Mock.ofType<IWorkspaceService>().object);
     serviceManager.addSingletonInstance<IProcessService>(IProcessService, process.object);
 
     const venvManager = new VirtualEnvironmentManager(serviceContainer);
